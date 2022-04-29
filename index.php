071b678793c31bf8f9fad4fa0ced58d17d8b41a0
acttoz <acttoze@gmail.com>
2022-04-29 15:03:03 +0900

    m

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <!DOCTYPE html>
 <html lang="ko">
+<h1>
+    5학년 4반 코딩 동아리
+</h1>
+<br />
+<hr />
 
-<a href="https://deokjeonge.github.io/1">1번</a>
-         </html>
+<h2>
+    개인 홈페이지 바로가기
+</h2>
+
+
+
+<?php for ($i = 0; $i < 24; $i++) { ?>
+    <a target="_blank" href="https://deokjeonge.github.io/<?=$i?>"><?=$i?>번</a>
+<? } ?>
+</script>
+
+</html>
